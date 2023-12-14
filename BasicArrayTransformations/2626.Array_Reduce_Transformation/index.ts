6df673a27d6fc3c57f3c4b9fcf6216f3945be08a
Abu Bakar <abubakarzafar2029@gmail.com>
2023-12-14 05:36:45 +0500

    Day 7 Problem 2626 solved

---
+++ BasicArrayTransformations/2626.Array_Reduce_Transformation/index.ts
@@ -0,0 +1,34 @@
+type Fn1 = (accum: number, curr: number) => number;
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+  if (nums.length === 0) {
+    return init;
+  }
+  let val: number = init;
+  for (let i = 0; i < nums.length; i++) {
+    val = fn(val, nums[i]);
+    console.log(
+      "Iteration number",
+      i,
+      "value is ",
+      val,
+      "And array element is",
+      nums[i]
+    );
+  }
+  return val;
+}
+
+// const nums = [1, 2, 3, 4];
+// let fn = function sum(accum, curr) {
+//   return accum + curr;
+// };
+// const init = 0;
+
+// 2nd failed test case
+const nums = [1, 2, 3, 4];
+let fn = function sum(accum, curr) {
+  return accum + curr * curr;
+};
+let init=100;
+console.log(reduce(nums, fn, init));
