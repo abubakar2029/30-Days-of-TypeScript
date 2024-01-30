79c1d720a0091b93221811b94809d1a5469cb688
Abu Bakar <abubakarzafar2029@gmail.com>
2024-01-30 21:40:22 +0500

    Memoization problem solved

--- Function_Transformations/2623.Memoize/index.ts
+++ Function_Transformations/2623.Memoize/index.ts
@@ -1,22 +1,32 @@
 type Fn2 = (...params: number[]) => number;
+interface CacheInterface {
+  [key: string]: number;
+}
 function memoize(fn: Fn2): Fn2 {
+  let cache: CacheInterface = {};
   return function (...args) {
-    console.log("ya args ma aya : ", args);
-    let response = fn(...args);
-    console.log("Remote functon ka respone : ", response);
-
-    return 2;
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    const result = fn(...args);
+    cache[key] = result;
+    return result;
   };
 }
-
-console.log("In the file");
 
 let callCount = 0;
 const memoizedFn = memoize(function (a, b) {
   callCount += 1;
+  console.log("callCount", callCount);
+
   return a + b;
 });
 memoizedFn(2, 3);
+memoizedFn(2, 2);
+memoizedFn();
+memoizedFn(1, 2);
+memoizedFn();
 /**
  * let callCount = 0;
  * const memoizedFn = memoize(function (a, b) {
@@ -27,3 +37,5 @@ memoizedFn(2, 3);
  * memoizedFn(2, 3) // 5
  * console.log(callCount) // 1
  */
+
+//  [4,4,1,3,2]
