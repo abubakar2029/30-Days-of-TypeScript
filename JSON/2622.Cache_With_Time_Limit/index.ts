31a44c3672687b819a3a278e3457a86992c817e9
Abu Bakar <abubakarzafar2029@gmail.com>
2024-02-22 21:33:30 +0500

    cache with time limit has one error left

---
+++ JSON/2622.Cache_With_Time_Limit/index.ts
@@ -0,0 +1,68 @@
+class TimeLimitedCache {
+  private cache: Map<number, { value: number; expiration: number }> = new Map();
+  // constructor() {}
+
+  set(key: number, value: number, duration: number): boolean {
+    const existingEntry = this.cache.get(key);
+    // if (this.cache.has(key) && existingEntry.expiration > Date.now()) {
+    if (existingEntry && existingEntry.expiration > Date.now()) {
+      return true;
+    } else {
+      this.cache.set(key, { value, expiration: duration + Date.now() });
+      return false;
+    }
+  }
+
+  get(key: number): number {
+    const existingEntry = this.cache.get(key);
+
+    if (
+      // this.cache.has(key) &&
+      existingEntry &&
+      existingEntry.expiration > Date.now()
+    ) {
+      return existingEntry.value;
+    } else return -1;
+  }
+
+  count(): number {
+    let count: number = 0;
+    for (let entry of this.cache.entries()) {
+      if (entry[1].expiration > Date.now()) count++;
+    }
+    return count;
+  }
+}
+
+
+// class TimeLimitedCache {
+//   private data: Map<number, { value: number, expiration: number }> = new Map();
+
+//   set(key: number, value: number, duration: number): boolean {
+//     if (this.data.has(key) && this.data.get(key).expiration > Date.now()) {
+//       this.data.set(key, { value, expiration: Date.now() + duration });
+//       return true;
+//     } else {
+//       this.data.set(key, { value, expiration: Date.now() + duration });
+//       return false;
+//     }
+//   }
+
+//   get(key: number): number {
+//     if (this.data.has(key) && this.data.get(key).expiration > Date.now()) {
+//       return this.data.get(key).value;
+//     } else {
+//       return -1;
+//     }
+//   }
+
+//   count(): number {
+//     let count = 0;
+//     for (const key of this.data.keys()) {
+//       if (this.data.get(key).expiration > Date.now()) {
+//         count++;
+//       }
+//     }
+//     return count;
+//   }
+// }
