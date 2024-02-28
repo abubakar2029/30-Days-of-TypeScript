eeb0e6f86cc24d615ee06bec971cb46dc4fc2f73
Abu Bakar <abubakarzafar2029@gmail.com>
2024-02-28 22:44:50 +0500

    Solution complete 2766

--- JSON/2724.Sort_By/index.ts
+++ JSON/2724.Sort_By/index.ts
@@ -1,6 +1,12 @@
-type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
-type Fn = (value: JSONValue) => number
+type JSONValue9 =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue9[]
+  | { [key: string]: JSONValue9 };
+type Fn9 = (value: JSONValue9) => number;
 
-function sortBy(arr: JSONValue[], fn: Fn): JSONValue[] {
-    
-};
+function sortBy(arr: JSONValue9[], Fn9: Fn9): JSONValue9[] {
+  return arr.sort((a, b) => Fn9(a) - Fn9(b));
+}
