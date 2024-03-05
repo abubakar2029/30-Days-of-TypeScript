deb60c72489fd41d70a83fde48e7d035a9707a75
Abu Bakar <abubakarzafar2029@gmail.com>
2024-03-05 19:56:02 +0500

    1st test case passed still there is time error in 2nd and 3rd

--- Promises/2721.Exxecute_Asynchronus_In_Parallel/index.ts
+++ Promises/2721.Exxecute_Asynchronus_In_Parallel/index.ts
@@ -1,9 +1,30 @@
-type Fn<T> = () => Promise<T>;
+type Fn99<T> = () => Promise<T>;
 
-function promiseAll<T>(functions: Fn<T>[]): Promise<T[]> {
-  let promises = functions.map((f) => f());
-  
+let wait = () => {};
+function promiseAll<T>(functions: Fn99<T>[]): Promise<T[]> {
+  return new Promise(async (res, rej) => {
+    let results: T[] = [];
+    let r;
+    for (let i = 0; i < functions.length; i++) {
+      r = await functions[i]()
+        .then((r) => {
+          results.push(r);
+          if (i === functions.length - 1) res(results);
+        })
+        .catch((e) => {
+          rej(e);
+        });
+    }
+  });
 }
+
+// By Using Promise.All
+// type Fn<T> = () => Promise<T>;
+
+// function promiseAll<T>(functions: Fn<T>[]): Promise<T[]> {
+//     const promises = functions.map(func => func()); // Invoke functions to get promises
+//     return Promise.all(promises);
+// }
 
 /**
  * const promise = promiseAll([() => new Promise(res => res(42))])
