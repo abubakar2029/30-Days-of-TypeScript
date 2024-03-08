5957cb0d17a57104e5c2ee6f013b06df187c5717
Abu Bakar <abubakarzafar2029@gmail.com>
2024-03-08 09:41:52 +0500

    Types Modified in problem 2722

--- JSON/2722.Join_Two_Arrays_By_Id/index.ts
+++ JSON/2722.Join_Two_Arrays_By_Id/index.ts
@@ -7,26 +7,26 @@ type JSONValue87 =
   | { [key: string]: JSONValue87 };
 type ArrayType = { id: number } & Record<string, JSONValue87>;
 
-type Item = Object & {
-    [key: string]: string
-}
-type Result =Object & { [key: string]: JSONValue87 };
+type Item = Record<string, string>;
+
+// type Result = { [key: string]: JSONValue87 };
 function join(arr1: ArrayType[], arr2: ArrayType[]): ArrayType[] {
-  let result: Record<string,Result> = {};
+  let result: Record<number, ArrayType> = {};
   //   Joining
-  arr1.forEach((item) => {
+  arr1.forEach((item: ArrayType) => {
     result[item.id] = item;
   });
   //   Insertion
-  arr2.forEach((item: any) => {
+  arr2.forEach((item: ArrayType) => {
     if (result[item.id]) {
-      Object.keys(item).forEach((key: any) => {
+      Object.keys(item).forEach((key: string) => {
         if (key in item) {
           result[item.id][key] = item[key];
-        }});
+        }
+      });
     } else {
-        result[item.id] = item;
+      result[item.id] = item;
     }
-});
-  return Object.values(result) as ArrayType[];
+  });
+  return Object.values(result);
 }
