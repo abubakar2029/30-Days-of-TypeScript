96be6db882763aefe3d8838029d95a70b90db135
Abu Bakar <abubakarzafar2029@gmail.com>
2024-03-26 00:50:15 +0500

    flatten array correct solution

--- JSON/2625.Flatten_Array/index.ts
+++ JSON/2625.Flatten_Array/index.ts
@@ -4,21 +4,18 @@ let flat = function (
   arr: MultiDimensionalArray,
   n: number
 ): MultiDimensionalArray {
-  if (n == 0) return arr;
-  let depth = 0;
+  if (n <= 0) return arr;
   let flattenArr: MultiDimensionalArray = [];
-  function flatOpt(ARR: MultiDimensionalArray) {
+  function flatOpt(ARR: MultiDimensionalArray, depth: number) {
     for (let i = 0; i < ARR.length; i++) {
-      if (Array.isArray(ARR[i]) && depth <= n) {
-        depth++;
-        flatOpt(ARR[i] as MultiDimensionalArray);
+      if (Array.isArray(ARR[i]) && depth < n) {
+        flatOpt(ARR[i] as MultiDimensionalArray, depth+1);
       } else {
         flattenArr.push(ARR[i]);
       }
-      depth = 0;
     }
   }
-  flatOpt(arr);
+  flatOpt(arr, 0);
 
   return flattenArr;
 };
